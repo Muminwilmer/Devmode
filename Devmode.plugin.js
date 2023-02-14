e643ee868a52a2aef659bab3bc0de635452f7658
Aglomation <113240095+Muminwilmer@users.noreply.github.com>
2023-02-14 17:13:49 +0100

    Update Devmode.plugin.js

--- Devmode.plugin.js
+++ Devmode.plugin.js
@@ -47,6 +47,18 @@ const config = {
 		//Occurs when plugin is turned off in settings
 	  stop() {
 	  	BdApi.alert("Turned Off", `Goodbye, Use Sealflix Please`);
+			let wpRequire;
+			window.webpackChunkdiscord_app.push([[ Math.random() ], {}, (req) => { wpRequire = req; }]);
+			mod = Object.values(wpRequire.c).find(x => typeof x?.exports?.Z?.isDeveloper !== "undefined");
+			usermod = Object.values(wpRequire.c).find(x => x?.exports?.default?.getUsers)
+			nodes = Object.values(mod.exports.Z._dispatcher._actionHandlers._dependencyGraph.nodes)
+			try {
+			    nodes.find(x => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({user: {flags: 0}})
+			} catch (e) {}
+			oldGetUser = usermod.exports.default.__proto__.getCurrentUser;
+			usermod.exports.default.__proto__.getCurrentUser = () => ({isStaff: () => false})
+			nodes.find(x => x.name == "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]()
+			usermod.exports.default.__proto__.getCurrentUser = oldGetUser
 			console.log("Plugin stopped successfully...");
 	  }
 		
